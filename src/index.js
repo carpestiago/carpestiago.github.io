9cc79f8d67c3d2d322fb8b8696a235d5e02ad131
Tiago Carpes <carpestiago@gmail.com>
2021-08-24 10:05:10 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Rotas from './routes';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<Rotas />, document.getElementById('root'));
+
+reportWebVitals();
